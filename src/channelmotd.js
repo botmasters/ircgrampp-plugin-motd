086ff8ba102de89621c4bd82652f2a0d91bcb530
exos <oscar@gentisoft.com>
2017-10-24 19:32:34 -0300

    First commit

---
+++ src/channelmotd.js
@@ -0,0 +1,158 @@
+
+import path from 'path';
+import {readFileSync} from 'fs';
+import escape from 'escape-html';
+
+const DELAY_TIME = 1000;
+const MOTD_USE_HTML = false;
+const MOTD_MESSAGE = `Welcome %u to %c`;
+
+const FIRST_NAME = 'first_name';
+const LAST_NAME = 'last_name';
+const INVITE_LINK = 'invite_link';
+const IS_BOT = 'is_bot';
+
+const tryFile = function tryFile(file) {
+    if (!file || typeof file !== 'string') {
+        return undefined;
+    }
+
+    return readFileSync(path.resolve(file), {
+        encoding: 'utf8',
+    });
+}
+
+export default class ChannelMotd {
+
+    constructor(channel, data, config, debug) {
+        this._channel = channel; 
+        this._data = data;
+        this._config = Object.assign({
+            motd: MOTD_MESSAGE,
+            motdFile: null,
+            html: MOTD_USE_HTML,
+            delayTime: DELAY_TIME,
+        }, config);
+        this._newusers = [];
+        this._debug = debug;
+
+        this._motd = {
+            singular: null,
+            plural: null,
+        };
+
+        this.debug(`Creating motd wrapper for ${channel.name}`);
+        this._loadMotd();
+        this._handleChannel();
+    }
+
+    debug(...args) {
+        this._debug(...args);
+    }
+
+    update(data) {
+        this._data = data;
+    }
+
+    _loadMotd() {
+        let {motd, motdFile} = this._config;
+
+        if (typeof motdFile !== 'object' || !motdFile) {
+            motdFile = {
+                singular: motdFile,
+                plural: motdFile,
+            };
+        }
+
+        if (typeof motd !== 'object' || !motd) {
+            motd = {
+                singular: motd,
+                plural: motd,
+            }
+        }
+
+        this._motd.singular = tryFile(motdFile.singular) || motd.singular; 
+        this._motd.plural = tryFile(motdFile.plural) || motd.plural; 
+    }
+
+    _handleChannel() {
+        this.debug("handle channel events");
+        this._channel.on('join', (_user) => {
+            let user = Object.assign({}, _user);
+
+            this.debug("User join", user.id);
+
+            if (user[IS_BOT]) {
+                this.debug('New user is a bot, ignoring to retard the skynet');
+                return;
+            }
+
+            this._newusers.push(user);
+            this.showMotdAsap();
+        });
+    }
+
+    parseMessage() {
+        let message = "";
+
+        if (this._newusers.length > 1) {
+            message = this._motd.plural;
+        } else {
+            message = this._motd.singular;
+        }
+
+        let names = this._newusers.map((u) => {
+            let text;
+
+            if (u.username) {
+                text = `@${u.username}`; 
+            }
+
+            text = `${u[FIRST_NAME]} ${u[LAST_NAME]}`;
+
+            if (this._config.html) {
+                return `<a href="tg://user?id=${u.id}">${escape(text)}</a>`;
+            } else {
+                return `[${text}](tg://user?id=${u.id})`;
+            }
+        });
+
+        let inviteLink = this._data[INVITE_LINK] || "";
+
+        return message
+                    .replace(/%u/g, names.join(', '))
+                    .replace(/%c/g, this._data.title)
+                    .replace(/%l/g, inviteLink)
+            ;
+    }
+
+    showMotd() {
+        let message = this.parseMessage();
+        this._newusers = [];
+
+        this.debug('Sending motd', message);
+        this._channel.sendMessage(message, {
+            "parse_mode": this._config.html ? 'HTML' : 'Markdown',
+            "disable_web_page_preview": true,
+            "disable_notification": true,
+        });
+    }
+
+    showMotdAsap() {
+        if (this._ts) {
+            clearTimeout(this._ts);
+        } 
+
+        this._ts = setTimeout(() => {
+            this.showMotd();
+        }, this._config.delayTime);
+    }
+
+    get name() {
+        return this._data.name;
+    }
+
+    get id() {
+        return this._channel.id;
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,58 @@
+
+import PluginBase from 'ircgrampp-plugin';
+import ChannelMotd from './channelmotd';
+
+export default class MotdPlugin extends PluginBase {
+
+    initialize() {
+        this.debug('Motd plugin initialized');
+        this._channels = [];
+    }
+
+    getCompatibleVersion() {
+        return "~0.3.0";
+    }
+
+    updateChannel(channel, data, config) {
+        let chan = this._channels.find(x => x.id === data.id);
+
+        if (chan) {
+            this.debug(`Channel info update to ${data.name}`);
+            chan.update(data);
+        } else {
+            this.debug(`New channel to use`, channel.name);
+            this._channels.push(new ChannelMotd(channel, data, config, (...args) => {
+                this.debug(...args);
+            }));
+        }
+    }
+
+    handleBridge(bridge, config) {
+        const channel = bridge._telegramChannel;
+        this.debug('Subscribe to updateinformtion of telegram channel');
+
+        channel.on('updateinformation', (data) => {
+            this.debug('data', data);
+            this.updateChannel(channel, data, config);
+        });
+
+    }
+
+    afterBridgeCreate(bridge) {
+        this.debug('New bridge ', bridge.name);
+
+        let conf = this.config.get(bridge.name);
+
+        if (conf) {
+            if (!conf.enable) {
+                this.debug('Motd is not enabled for this bridge, pass');
+                return;
+            }
+
+            this.handleBridge(bridge, conf);
+        } else {
+            this.debug('There are not configuration for bridge, pass');
+        }
+    }
+
+}
